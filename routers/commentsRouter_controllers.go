144be060fa9ec9c1e10b1e131e53ff66250e7c06
Jonathan Steven Capera Quintana <jscaperaq@correo.udistrital.edu.co>
2020-11-17 09:52:05 -0500

    feat: Generación api beego parametros con variables de entorno #1

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,235 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:AreaTipoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:AreaTipoController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: "/",
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:AreaTipoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:AreaTipoController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: "/",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:AreaTipoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:AreaTipoController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:AreaTipoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:AreaTipoController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:AreaTipoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:AreaTipoController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: "/",
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: "/",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroPeriodoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroPeriodoController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: "/",
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroPeriodoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroPeriodoController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: "/",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroPeriodoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroPeriodoController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroPeriodoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroPeriodoController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroPeriodoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:ParametroPeriodoController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:PeriodoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:PeriodoController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: "/",
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:PeriodoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:PeriodoController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: "/",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:PeriodoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:PeriodoController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:PeriodoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:PeriodoController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:PeriodoController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:PeriodoController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:TipoParametroController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:TipoParametroController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: "/",
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:TipoParametroController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:TipoParametroController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: "/",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:TipoParametroController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:TipoParametroController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:TipoParametroController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:TipoParametroController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:TipoParametroController"] = append(beego.GlobalControllerRouter["github.com/JocoolQ/parametros/controllers:TipoParametroController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: "/:id",
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
