0cba9dca82beee74c2334e46a5ac9e64561f1fa5
Jhon Gabriel Castellanos Jimenez <corio27@gmail.com>
2020-12-17 21:40:47 -0500

    ajuste de variables según lineamiento
    ajuste de variables según lineamiento

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export PARAMETROS__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/parametros_crud/db/username --output text --query Parameter.Value)"
-  export PARAMETROS__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/parametros_crud/db/password --output text --query Parameter.Value)"
+  export PARAMETROS_CRUD_PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/parametros_crud/db/username --output text --query Parameter.Value)"
+  export PARAMETROS_CRUD_PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/parametros_crud/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
